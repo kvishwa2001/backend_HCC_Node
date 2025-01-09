a798513839f85db6e0a215a2fcad9691c805bfe7
K Vishwa <kvishwa.com@gmail.com>
2025-01-09 15:57:15 +0530

    Update Database.js

--- config/Database.js
+++ config/Database.js
@@ -1,13 +1,23 @@
 const mysql = require("mysql2")
 
+// old  const connection = mysql.createPool({
+//     host:'localhost',
+//     user:"root",
+//     password:"9876543210",     
+//     database:"collection",
+//     waitForConnections:true,
+//     connectionLimit:100,
+//     queueLimit:0
+// });
+
 const connection = mysql.createPool({
-    host:'localhost',
-    user:"root",
-    password:"9876543210",     
-    database:"collection",
-    waitForConnections:true,
-    connectionLimit:100,
-    queueLimit:0
+    host: '111.118.177.178', // Use 'localhost' if running on the same server
+    user: "alpharhz_rootadmin",
+    password: "localhostroot",
+    database: "alpharhz_collection",
+    waitForConnections: true,
+    connectionLimit: 100,
+    queueLimit: 0
 });
 
 // host:'106.51.180.3',
